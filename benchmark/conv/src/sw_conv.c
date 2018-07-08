005d768b0ee27eafba6ca0ad1cdb9108cc83d661
harvardchen <harvardchen97@gmail.com>
2018-07-08 23:50:25 +0800

    autocmt: cloud_run USER=chencanyu HW_VAL=conv:01

--- benchmark/conv/src/sw_conv.c
+++ benchmark/conv/src/sw_conv.c
@@ -570,7 +570,8 @@ void pooling() {
                     {
                         //input_offset += mul(input_fm_w,(mul(y,stride)+py-pad)) + mul(x,stride)+px-pad;
 
-                        input_offset = mul(no,mul(input_fm_w,input_fm_h)) + mul(input_fm_w,(mul(y,stride)+py-pad)) + mul(x,stride) + px-pad;
+                        // input_offset = mul(no,mul(input_fm_w,input_fm_h)) + mul(input_fm_w,(mul(y,stride)+py-pad)) + mul(x,stride) + px-pad;
+                        input_offset = mul(no,mul(input_fm_w,input_fm_h)) + mul(input_fm_w,y+py) + x + px;
                         printf("%d ", *(out+input_offset));
 
                         // if (((mul(stride,x) + px) >= pad) && ((mul(stride,x) + px) <= (pad+input_fm_w)) 
